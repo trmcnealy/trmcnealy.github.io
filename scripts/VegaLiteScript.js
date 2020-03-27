8275563d20d5680b909f8b224953a39e3d327f13
Timothy McNealy <timothy.mcnealy@gmail.com>
2020-03-27 12:33:08 -0500

    Update VegaLiteScript.js

--- scripts/VegaLiteScript.js
+++ scripts/VegaLiteScript.js
@@ -120,7 +120,7 @@
                     }
                 }
 
-                var address = dotnet_script.src.substring(0, dotnet_script.src.length - 31);
+                var address = dotnet_script.substring(0, dotnet_script.src.length - 31);
 
                 createDotnetInteractiveClient(address).then(function (interactive) {
 
