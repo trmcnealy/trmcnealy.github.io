8d68b8e3304e5d032d189e837290893b8c645633
Timothy McNealy <timothy.mcnealy@gmail.com>
2020-04-03 09:03:35 -0500

    Update vega-arrow-transforms.js

--- scripts/vega-arrow-transforms.js
+++ scripts/vega-arrow-transforms.js
@@ -1,1 +1,144 @@
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports,require("vega-dataflow"),require("apache-arrow")):"function"==typeof define&&define.amd?define(["exports","vega-dataflow","apache-arrow"],t):t(((e=e||self).vega=e.vega||{},e.vega.transforms=e.vega.transforms||{},e.vega.transforms.arrow={}),e.vegaDataflow,e.apacheArrow)}(this,(function(e,t,a){"use strict";var n=window&&window.__awaiter||function(e,t,a,n){return new(a||(a=Promise))((function(r,i){function s(e){try{l(n.next(e))}catch(e){i(e)}}function o(e){try{l(n.throw(e))}catch(e){i(e)}}function l(e){var t;e.done?r(e.value):(t=e.value,t instanceof a?t:new a((function(e){e(t)}))).then(s,o)}l((n=n.apply(e,t||[])).next())}))};class r extends t.Transform{constructor(e){super([],e),this._dataTable=null,this._definition=null,t.definition({type:"ArrowTransform",metadata:{changes:!0,source:!0},params:[{name:"filter",type:"Predicate"},{name:"getColumn",type:"field"},{name:"getColumnAt",type:"number"},{name:"select",type:"field",array:!0},{name:"selectAt",type:"number",array:!0},{name:"slice",type:"param",params:[{name:"begin",type:"number"},{name:"end",type:"number"}]}]})}get Table(){return this._dataTable}set Table(e){e instanceof a.Table&&(this._dataTable=e),e instanceof ArrayBuffer&&(e=new Uint8Array(e),this._dataTable=a.Table.from(Array.isArray(e)?e:[e])),this._dataTable=a.Table.new(e)}get Definition(){return this._definition}set Definition(e){this._definition=e}transform(e,a){return n(this,void 0,void 0,(function*(){if(!this._dataTable)throw Error("ArrowTransform missing data Table.");const n=a.fork(a.NO_FIELDS&a.NO_SOURCE);let r=e.filter,i=e.getColumn,s=e.getColumnAt,o=e.select,l=e.selectAt,d=e.slice,f=null;return r?f=yield this._dataTable.filter(r):i?f=yield this._dataTable.getColumn(i):s?f=yield this._dataTable.getColumnAt(s):o?f=yield this._dataTable.select(o):l?f=yield this._dataTable.selectAt(l):d&&(f=yield this._dataTable.slice(d.get("begin"),d.get("end"))),f.forEach(t.ingest),n.rem=t.Transform.value,t.Transform.value=n.add=n.source=f,n}))}}e.Slice=class{constructor(e,t){this._begin=0,this._end=0,this._begin=e,this._end=t}get begin(){return this._begin}set begin(e){this._begin=e}get end(){return this._end}set end(e){this._end=e}},e.register=function(e){"undefined"===e||e.transforms.ArrowFilter||(e.transforms.ArrowFilter=r)},Object.defineProperty(e,"__esModule",{value:!0})}));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('vega-dataflow'), require('apache-arrow')) :
+    typeof define === 'function' && define.amd ? define(['exports', 'vega-dataflow', 'apache-arrow'], factory) :
+    (global = global || self, factory((global.vega = global.vega || {}, global.vega.transforms = global.vega.transforms || {}, global.vega.transforms.arrow = {}), global.vegaDataflow, global.apacheArrow));
+}(this, (function (exports, vegaDataflow, apacheArrow) { 'use strict';
+
+    // ReSharper disable TsResolvedFromInaccessibleModule
+    // ReSharper disable QualifiedExpressionMaybeNull
+    var __awaiter = (window && window.__awaiter) || function (thisArg, _arguments, P, generator) {
+        function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+            function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+            function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+            step((generator = generator.apply(thisArg, _arguments || [])).next());
+        });
+    };
+    //arrow.predicate.col('precipitation').eq(0)
+    class Slice {
+        constructor(begin, end) {
+            this._begin = 0;
+            this._end = 0;
+            this._begin = begin;
+            this._end = end;
+        }
+        get begin() {
+            return this._begin;
+        }
+        set begin(value) {
+            this._begin = value;
+        }
+        get end() {
+            return this._end;
+        }
+        set end(value) {
+            this._end = value;
+        }
+    }
+    class ArrowTransform extends vegaDataflow.Transform {
+        constructor(params) {
+            super([], params);
+            this._dataTable = null;
+            this._definition = null;
+            vegaDataflow.definition({
+                type: "ArrowTransform",
+                metadata: { changes: true, source: true },
+                params: [
+                    { name: "filter", type: "Predicate" },
+                    { name: "getColumn", type: "field" },
+                    { name: "getColumnAt", type: "number" },
+                    { name: "select", type: "field", "array": true },
+                    { name: "selectAt", type: "number", "array": true },
+                    { name: "slice", type: "param", "params": [{ name: "begin", type: "number" }, { name: "end", type: "number" }] }
+                ]
+            });
+        }
+        get Table() {
+            return this._dataTable;
+        }
+        set Table(table_data) {
+            if (table_data instanceof apacheArrow.Table) {
+                this._dataTable = table_data;
+            }
+            if (table_data instanceof ArrayBuffer) {
+                table_data = new Uint8Array(table_data);
+                this._dataTable = apacheArrow.Table.from(Array.isArray(table_data) ? table_data : [table_data]);
+            }
+            this._dataTable = apacheArrow.Table.new(table_data);
+        }
+        get Definition() {
+            return this._definition;
+        }
+        set Definition(value) {
+            this._definition = value;
+        }
+        transform(_, pulse) {
+            return __awaiter(this, void 0, void 0, function* () {
+                if (!this._dataTable) {
+                    throw Error("ArrowTransform missing data Table.");
+                }
+                const out = pulse.fork(pulse.NO_FIELDS & pulse.NO_SOURCE);
+                let filter = _.filter;
+                let getColumn = _.getColumn;
+                let getColumnAt = _.getColumnAt;
+                let select = _.select;
+                let selectAt = _.selectAt;
+                let slice = _.slice;
+                let result = null;
+                if (filter) {
+                    result = yield this._dataTable.filter(filter);
+                }
+                else if (getColumn) {
+                    result = yield this._dataTable.getColumn(getColumn);
+                }
+                else if (getColumnAt) {
+                    result = yield this._dataTable.getColumnAt(getColumnAt);
+                }
+                else if (select) {
+                    result = yield this._dataTable.select(select);
+                }
+                else if (selectAt) {
+                    result = yield this._dataTable.selectAt(selectAt);
+                }
+                else if (slice) {
+                    result = yield this._dataTable.slice(slice.get("begin"), slice.get("end"));
+                }
+                result.forEach(vegaDataflow.ingest);
+                out.rem = vegaDataflow.Transform.value;
+                vegaDataflow.Transform.value = out.add = out.source = result;
+                return out;
+            });
+        }
+        ;
+    }
+    function register(Vega) {
+        if ("undefined" !== Vega && !Vega.transforms["ArrowFilter"]) {
+            Vega.transforms["ArrowFilter"] = ArrowTransform;
+        }
+    }
+    // TODO
+    //Map 
+    //    hours = (function () {
+    //        const arr = [];
+    //        for (const x of rainfall.getColumn('date')) {
+    //            arr.push(x.getHours())
+    //        }
+    //        return arr;
+    //    }())
+    //Scan
+    //    (function () {
+    //        const vec = rainfall.getColumn('precipitation')
+    //        let highIdx = 0;
+    //        rainfall.scan((idx) => { highIdx = vec.get(idx) > vec.get(highIdx) ? idx : highIdx; })
+    //        const row = rainfall.get(highIdx)
+    //        return `${row.date} => ${row.precipitation} inches`
+    //    }())
+
+    exports.Slice = Slice;
+    exports.register = register;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=vega-arrow-transforms.js.map
